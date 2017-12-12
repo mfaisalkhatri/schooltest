b4446f45f58180b0820c123f3e68b4419665c73e
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2017-12-12 20:23:22 +0530

    Made changes for selector class

--- src/main/java/com/SchoolMgmt/utils/Selector.java
+++ src/main/java/com/SchoolMgmt/utils/Selector.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class Selectors {
+public class Selector {
 
 	private WebDriver driver;
 
 
-	public Selectors(WebDriver driver) {
+	public Selector(WebDriver driver) {
 		
 		this.driver = driver;
 	}
